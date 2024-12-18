a798cff8f8553b042a5e2b3ccb5a1637acf6ee51
Ahmad Muhajir <muhajirgood05@gmail.com>
2024-12-18 20:46:35 +0700

    adding data (aprill) manually

---
+++ src/components/RevenueChart.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="chart-container">
+    <Bar :data="chartData" :options="chartOptions" />
+  </div>
+</template>
+
+<script>
+// import { defineComponent } from "vue";
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+
+// Register chart.js components
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+
+export default ({
+  name: "RevenueChart",
+  components: {Bar,
+  },
+  props: {
+    salesData: {
+      type: Array,
+      required: true,
+    },
+    expenseData: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    chartData() {
+      return {
+        labels: ["Januari", "Februari", "Maret", "April"], // Sesuaikan label dengan data
+        datasets: [
+          {
+            label: "Sales",
+            backgroundColor: "#4caf50",
+            data: this.salesData,
+          },
+          {
+            label: "Expenses",
+            backgroundColor: "#f44336",
+            data: this.expenseData,
+          },
+        ],
+      };
+    },
+    chartOptions() {
+      return {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: true,
+            position: "top",
+          },
+        },
+      };
+    },
+  },
+});
+
+
+
+</script>
+
+
+<style scoped>
+.chart-container {
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/data/data.js
@@ -0,0 +1,11 @@
+export const transactions = [
+  { id: 1, type: "sale", amount: 12500, date: "2024-12-01" },
+  { id: 2, type: "sale", amount: 35000, date: "2024-12-02" },
+  { id: 3, type: "sale", amount: 25000, date: "2024-12-03" },
+  { id: 3, type: "sale", amount: 15000, date: "2024-12-04" },
+  { id: 4, type: "expense", amount: 5000, date: "2024-12-01" },
+  { id: 5, type: "expense", amount: 9000, date: "2024-12-02" },
+  { id: 6, type: "expense", amount: 4000, date: "2024-12-03" },
+  { id: 6, type: "expense", amount: 10000, date: "2024-12-04" },
+
+];
